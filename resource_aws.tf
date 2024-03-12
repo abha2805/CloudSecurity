653585ecb7a094e8c08176cccfa53ea97faac9c0
Abha Kamble <abhakamble28@gmail.com>
2024-03-11 22:58:04 -0400

    Update resource_aws.tf

--- resource_aws.tf
+++ resource_aws.tf
@@ -11,17 +11,17 @@ resource "aws_security_group" "sg" {
   }
 }
 
-resource "aws_iam_instance_profile" "my_profile" {
+/*resource "aws_iam_instance_profile" "my_profile" {
   name = "my_profile"
   role = aws_iam_role.first_role.name
-}
+}*/
 
 resource "aws_instance" "instance_1" {
   ami           = "ami-064573ac645743ea8"
   instance_type = "t2.micro"
   subnet_id      = aws_subnet.subnet.id  
   vpc_security_group_ids = [aws_security_group.sg.id]
-  iam_instance_profile = aws_iam_instance_profile.my_profile.name
+  //iam_instance_profile = aws_iam_instance_profile.my_profile.name
 
   tags = {
     Name        = "ec2"
